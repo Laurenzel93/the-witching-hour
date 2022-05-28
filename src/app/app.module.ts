85d7637f2aab761d86fb735292363e05f0790a15
Laurenzel93 <Laurenzel93@aol.com>
2022-05-27 21:23:05 -0400

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './components/cart/cart.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { InventoryItemDetailsComponent } from './components/inventory-item-details/inventory-item-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    NavbarComponent,
+    HomepageComponent,
+    InventoryItemDetailsComponent
   ],
   imports: [
     BrowserModule,
